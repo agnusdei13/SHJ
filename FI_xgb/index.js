066861afa2c3d27d86ae380af2770f4e62cb5cd3
agnusdei13 <33951756+agnusdei13@users.noreply.github.com>
2025-07-13 21:53:13 +0900

    Update index.js

--- FI_xgb/index.js
+++ FI_xgb/index.js
@@ -1,44 +1,29 @@
 async function runExample() {
+    let x = [];
+    for (let i = 0; i < 12; i++) {
+        x[i] = parseFloat(document.getElementById(`box${i+1}`).value);
+    }
 
-    var x = [];
-
-     x[0] = document.getElementById('box1').value;
-     x[1] = document.getElementById('box2').value;
-     x[2] = document.getElementById('box3').value;
-     x[3] = document.getElementById('box4').value;
-     x[4] = document.getElementById('box5').value;
-     x[5] = document.getElementById('box6').value;
-     x[6] = document.getElementById('box7').value;
-     x[7] = document.getElementById('box8').value;
-     x[8] = document.getElementById('box9').value;
-     x[9] = document.getElementById('box10').value;
-     x[10] = document.getElementById('box11').value;
-     x[11] = document.getElementById('box12').value;
-
-        let tensorX = new ort.Tensor('float32', x, [1, 12] );
-
-
-    let session = await ort.InferenceSession.create('xgb_FI.onnx');
+    const tensorX = new ort.Tensor('float32', x, [1, 12]);
+    const session = await ort.InferenceSession.create('xgb_FI.onnx');
     
-   const inputName = session.inputNames[0];
-    let feeds = {};
+    const inputName = session.inputNames[0];
+    const feeds = {};
     feeds[inputName] = tensorX;
 
-    let result = await session.run(feeds);
-
-    
+    const result = await session.run(feeds);
     const outputName = session.outputNames[0];
     let outputData = result[outputName].data[0];
-
     outputData = parseFloat(outputData).toFixed(2);
 
-    let predictions = document.getElementById('predictions');
+    const predictions = document.getElementById('predictions');
     predictions.innerHTML = `
         <hr> Got an output tensor with values: <br/>
         <table>
             <tr>
-                <td>Rating of Wine Quality</td>
+                <td>FI prediction</td>
                 <td id="td0">${outputData}</td>
             </tr>
         </table>
+    `;
 }
