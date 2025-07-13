dba54ab7df69a7df027441abbe2cf3896f9b59c6
agnusdei13 <33951756+agnusdei13@users.noreply.github.com>
2025-07-13 17:40:57 +0900

    Update index.js

--- FI_xgb/index.js
+++ FI_xgb/index.js
@@ -1,33 +1,38 @@
 async function runExample() {
-    let x = [];
 
-    for (let i = 0; i < 12; i++) {
-        x[i] = parseFloat(document.getElementById(`box${i + 1}`).value);
-    }
+    var x = new Float32Array( 1, 11 )
 
-    let tensorX = new ort.Tensor('float32', x, [1, 12]);
+    var x = [];
 
-    const session = await ort.InferenceSession.create('xgb_FI.onnx');
+     x[0] = document.getElementById('box1').value;
+     x[1] = document.getElementById('box2').value;
+     x[2] = document.getElementById('box3').value;
+     x[3] = document.getElementById('box4').value;
+     x[4] = document.getElementById('box5').value;
+     x[5] = document.getElementById('box6').value;
+     x[6] = document.getElementById('box7').value;
+     x[7] = document.getElementById('box8').value;
+     x[8] = document.getElementById('box9').value;
+     x[9] = document.getElementById('box10').value;
+     x[10] = document.getElementById('box11').value;
+     x[11] = document.getElementById('box12').value;
 
-    // Replace these names based on actual model IO
-    const inputName = session.inputNames[0];
-    const outputName = session.outputNames[0];
+    let tensorX = new ort.Tensor('float32', x, [0, 11] );
+    let feeds = {input: tensorX};
 
-    let feeds = {};
-    feeds[inputName] = tensorX;
+    let session = await ort.InferenceSession.create('xgboost_WineQuality_ort.onnx');
+    
+   let result = await session.run(feeds);
+   let outputData = result.output_label.data;
 
-    let result = await session.run(feeds);
-    let outputData = result[outputName].data[0];
+  outputData = parseFloat(outputData).toFixed(2)
 
-    let predictions = document.getElementById('predictions');
+   let predictions = document.getElementById('predictions');
 
-    predictions.innerHTML = `
-        <hr> Got an output tensor with values: <br/>
-        <table>
-            <tr>
-                <td>FI prediction</td>
-                <td id="td0">${outputData.toFixed(2)}</td>
-            </tr>
-        </table>
-    `;
-}
+  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
+   <table>
+     <tr>
+       <td>  Rating of Wine Quality  </td>
+       <td id="td0">  ${outputData}  </td>
+     </tr>
+  </table>`;
